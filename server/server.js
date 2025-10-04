55949887e26d63bd4b7b04bfe2e1680514398a84
Munish Verma <maniverma0420@gmail.com>
2025-10-04 14:59:38 +0530

    server js changes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,18 @@ const { body, validationResult } = require('express-validator');
 const config = require('./config');
 require('dotenv').config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
 const app = express();
 const PORT = config.PORT;
 
